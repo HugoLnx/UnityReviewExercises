c5179ee07082b659327f97d54e24d2dd168d525b
hugolnx <hugolnx@gmail.com>
2023-03-02 10:12:57 -0300

    Review Scenario for configuring an 2D environment

---
+++ Assets/Scenarios/SpritesCamera2D/Code/Scripts/MoveCameraEdgesSolution.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class MoveCameraEdgesSolution : MonoBehaviour
+{
+    private const float AspectRatio = 16f/9f;
+    private const float CameraHeight = 5.4f;
+    private const float CameraWidth = CameraHeight * AspectRatio;
+    [Button]
+    public void MoveUp()
+    {
+        transform.position = Vector3.up * CameraHeight;
+    }
+    [Button]
+    public void MoveDown()
+    {
+        transform.position = Vector3.down * CameraHeight;
+    }
+
+    [Button]
+    public void MoveRight()
+    {
+        transform.position = Vector3.right * CameraWidth;
+    }
+    [Button]
+    public void MoveLeft()
+    {
+        transform.position = Vector3.left * CameraWidth;
+    }
+    [Button]
+    public void MoveCenter()
+    {
+        transform.position = Vector3.zero;
+    }
+}
