8afaee7e229e51936fead002f81af040ff4e74a1
hugolnx <hugolnx@gmail.com>
2023-03-08 14:27:34 -0300

    SpriteCamera2D: Improve solution code by multiplying screen size and the axis vector

--- Assets/Scenarios/SpritesCamera2D/Code/Scripts/MoveCameraEdgesSolution.cs
+++ Assets/Scenarios/SpritesCamera2D/Code/Scripts/MoveCameraEdgesSolution.cs
@@ -6,32 +6,13 @@ using UnityEngine;
 public class MoveCameraEdgesSolution : MonoBehaviour
 {
     private const float AspectRatio = 16f/9f;
-    private const float CameraHeight = 2.7f;
-    private const float CameraWidth = CameraHeight * AspectRatio;
-    [Button]
-    public void MoveUp()
-    {
-        transform.position = Vector3.up * CameraHeight;
-    }
-    [Button]
-    public void MoveDown()
-    {
-        transform.position = Vector3.down * CameraHeight;
-    }
+    private const float ScreenHeight = 5.4f;
+    private const float ScreenWidth = ScreenHeight * AspectRatio;
+    private Vector2 ScreenSize => new(ScreenWidth, ScreenHeight);
 
-    [Button]
-    public void MoveRight()
+    private void Update()
     {
-        transform.position = Vector3.right * CameraWidth;
-    }
-    [Button]
-    public void MoveLeft()
-    {
-        transform.position = Vector3.left * CameraWidth;
-    }
-    [Button]
-    public void MoveCenter()
-    {
-        transform.position = Vector3.zero;
+        Vector2 input = new(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        transform.position = input * ScreenSize / 2f;
     }
 }
